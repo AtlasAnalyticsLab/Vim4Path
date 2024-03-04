9baab885872f26a4eeff7ea33b33251826af3087
Ali Nasiri Sarvi <a.nasiri1998@gmail.com>
2024-03-04 13:09:40 -0500

    added postiional embedding support for VIM

--- dino/vim/models_mamba.py
+++ dino/vim/models_mamba.py
@@ -488,7 +488,7 @@ from mamba_ssm.modules.mamba_simple import Mamba
 from mamba_ssm.utils.generation import GenerationMixin
 from mamba_ssm.utils.hf import load_config_hf, load_state_dict_hf
 
-from rope import *
+from .rope import *
 import random
 
 try:
@@ -844,10 +844,49 @@ class VisionMamba(nn.Module):
     def load_pretrained(self, checkpoint_path, prefix=""):
         _load_weights(self, checkpoint_path, prefix)
 
+    def interpolate_pos_encoding(self, x, w, h):
+        npatch = x.shape[1] - 1
+        N = self.pos_embed.shape[1] - 1
+        if npatch == N and w == h:
+            return self.pos_embed
+        class_pos_embed = self.pos_embed[:, 0]
+        patch_pos_embed = self.pos_embed[:, 1:]
+        dim = x.shape[-1]
+        w0 = w // self.patch_embed.patch_size[1]
+        h0 = h // self.patch_embed.patch_size[0]
+        # we add a small number to avoid floating point error in the interpolation
+        # see discussion at https://github.com/facebookresearch/dino/issues/8
+        w0, h0 = w0 + 0.1, h0 + 0.1
+        patch_pos_embed = nn.functional.interpolate(
+            patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(0, 3, 1, 2),
+            scale_factor=(w0 / math.sqrt(N), h0 / math.sqrt(N)),
+            mode='bicubic',
+        )
+        assert int(w0) == patch_pos_embed.shape[-2] and int(h0) == patch_pos_embed.shape[-1]
+        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
+        return torch.cat((class_pos_embed.unsqueeze(0), patch_pos_embed), dim=1)
+
+    def prepare_tokens(self, x):
+        B, nc, w, h = x.shape
+        x = self.patch_embed(x)  # patch linear embedding
+
+        # add the [CLS] token to the embed patch tokens
+        cls_tokens = self.cls_token.expand(B, -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # add positional encoding to each token
+        x = x + self.interpolate_pos_encoding(x, w, h)
+
+        return self.pos_drop(x)
+
+
     def forward_features(self, x, inference_params=None, if_random_cls_token_position=False, if_random_token_rank=False):
         # taken from https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
         # with slight modifications to add the dist_token
-        x = self.patch_embed(x)
+        if self.if_abs_pos_embed:
+            x = self.prepare_tokens(x)
+        else:
+            x = self.patch_embed(x)
         B, M, _ = x.shape
 
         if self.if_cls_token:
@@ -874,15 +913,6 @@ class VisionMamba(nn.Module):
                     x = torch.cat((cls_token, x), dim=1)
                 M = x.shape[1]
 
-        if self.if_abs_pos_embed:
-            # if new_grid_size[0] == self.patch_embed.grid_size[0] and new_grid_size[1] == self.patch_embed.grid_size[1]:
-            #     x = x + self.pos_embed
-            # else:
-            #     pos_embed = interpolate_pos_embed_online(
-            #                 self.pos_embed, self.patch_embed.grid_size, new_grid_size,0
-            #             )
-            x = x + self.pos_embed
-            x = self.pos_drop(x)
 
         if if_random_token_rank:
 

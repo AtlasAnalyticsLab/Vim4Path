7be898bd536dce2e9907e6f3a2de92baf731e8fb
Ali Nasiri Sarvi <a.nasiri1998@gmail.com>
2024-05-30 09:53:42 -0400

    updated check image code

--- preprocess/check_images.py
+++ preprocess/check_images.py
@@ -18,7 +18,15 @@ def check_and_remove_images(directory):
                 corrupted_count += 1
     return corrupted_count
 
-# The path to the directory containing your images
-directory_path = '/home/a_n29343/CHUM/VIM4Path/datasets/CHUM/output_vim512_small/'
-corrupted_images_count = check_and_remove_images(directory_path)
-print(f'Number of corrupted images removed: {corrupted_images_count}')
+
+def main():
+    parser = argparse.ArgumentParser(description="Check and remove corrupted images in a directory")
+    parser.add_argument('directory', type=str, help='The path to the directory containing the images')
+    
+    args = parser.parse_args()
+    
+    corrupted_images_count = check_and_remove_images(args.directory)
+    print(f'Number of corrupted images removed: {corrupted_images_count}')
+
+if __name__ == '__main__':
+    main()

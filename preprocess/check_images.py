6c917c6602ad48b247bce7f6e028410e4a239f23
Ali Nasiri Sarvi <a.nasiri1998@gmail.com>
2024-05-31 18:02:44 -0400

    fixed typo

--- preprocess/check_images.py
+++ preprocess/check_images.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image, ImageFile
 from tqdm import tqdm
+import argparse
 
 def check_and_remove_images(directory):
     corrupted_count = 0
@@ -21,11 +22,11 @@ def check_and_remove_images(directory):
 
 def main():
     parser = argparse.ArgumentParser(description="Check and remove corrupted images in a directory")
-    parser.add_argument('directory', type=str, help='The path to the directory containing the images')
+    parser.add_argument('--dir', type=str, help='The path to the directory containing the images')
     
     args = parser.parse_args()
     
-    corrupted_images_count = check_and_remove_images(args.directory)
+    corrupted_images_count = check_and_remove_images(args.dir)
     print(f'Number of corrupted images removed: {corrupted_images_count}')
 
 if __name__ == '__main__':
